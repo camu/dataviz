94ca0b8cc290f5a038dfe9be9c36c470b6f40348
camu <camu.umac@gmail.com>
2014-01-14 16:12:42 +0000

    some updates; mostly user-given sizes

--- dataviz.c
+++ dataviz.c
@@ -52,24 +52,37 @@ int pie_chart( struct tag *_taglist ) {
 	struct tag *container = _taglist, *style_tag = 0;
 	int parent_id = container->id;
 
+	const char delimiter = ';';
+	int w = 0, h = 0, b = 0;
+
 	float values_summed = 0;
 	while( (container = container->next) ) {
 		if( container->parent && container->parent->id == parent_id ) {
-			if( strcmp( container->class, "val" ) == 0 )
-				values_summed += atof( container->val );
 			if( strcmp( container->class, "fname" ) == 0 )
 				fp = fopen( container->val, "w" );
 			if( strcmp( container->class, "style" ) == 0 )
 				style_tag = container;
+			if( strcmp( container->class, "size" ) == 0 ) {
+				w = atoi( container->val );
+				char *tmp; if( !(tmp = strchr( container->val, delimiter )) ) return -1;
+				h = atoi( tmp+1 );
+				if( container->attr ) b = atoi( container->attr );
+			if( strcmp( container->class, "val" ) == 0 )
+				values_summed += atof( container->val );
+			}
 		}
 	}
+
+	if( w == 0 || h == 0 ) return -1;
 
 	if( !fp ) fp = stdout;
 
 	fprintf( fp, "<?xml version='1.0' ?>\n" );
 	if( style_tag )
 		fprintf( fp, "<?xml-stylesheet type='text/css' href='%s' ?>\n", style_tag->val );
-	fprintf( fp, "<svg xmlns='http://www.w3.org/2000/svg' width='1000' height='1000'>\n" );
+	fprintf( fp, "<svg xmlns='http://www.w3.org/2000/svg' width='%i' height='%i'>\n", w+2*b, h+2*b );
+
+	if( w < h ) h = w; w = h;
 
 	container = _taglist;
 
@@ -78,7 +91,7 @@ int pie_chart( struct tag *_taglist ) {
 	while( (container = container->next) ) {
 		if( container->parent && container->parent->id == parent_id && strcmp( container->class, "val" ) == 0 ) {
 			angle2 += atof( container->val )/values_summed*FULL_ANGLE;
-			sector( id, (container->attr ? container->attr : ""), 200, 200, 100, angle1, angle2, fp );
+			sector( id, (container->attr ? container->attr : ""), w/2+b, h/2+b, w/2, angle1, angle2, fp );
 			angle1 = angle2;
 			id++;
 		}
@@ -99,6 +112,8 @@ int line_graph( struct tag *_taglist ) {
 	struct tag *container = _taglist, *style_tag = 0;
 	int parent_id = container->id;
 
+	int w = 0, h = 0, b = 0;
+	float reso_x = 0, reso_y = 0;
 	float min_x = 0, min_y = 0, max_x = 0, max_y = 0;
 	while( (container = container->next) ) {
 		if( container->parent && container->parent->id == parent_id ) {
@@ -106,6 +121,17 @@ int line_graph( struct tag *_taglist ) {
 				fp = fopen( container->val, "w" );
 			if( strcmp( container->class, "style" ) == 0 )
 				style_tag = container;
+			if( strcmp( container->class, "size" ) == 0 ) {
+				w = atoi( container->val );
+				char *tmp; if( !(tmp = strchr( container->val, delimiter )) ) return -1;
+				h = atoi( tmp+1 );
+				if( container->attr ) b = atoi( container->attr );
+			}
+			if( strcmp( container->class, "reso" ) == 0 ) {
+				reso_x = atof( container->val );
+				char *tmp; if( !(tmp = strchr( container->val, delimiter )) ) return -1;
+				reso_y = atof( tmp+1 );
+			}
 			if( strcmp( container->class, "val" ) == 0 ) {
 				float val = atof( container->val );
 				if( ( min_x == 0 && val > 0 ) || val < min_x ) min_x = val;
@@ -117,15 +143,27 @@ int line_graph( struct tag *_taglist ) {
 		}
 	}
 
+	if( w == 0 || h == 0 ) return -1;
+	max_x += reso_x/2;
+	max_y += reso_y/2;
+	reso_x = reso_x/max_x*w;
+	reso_y = reso_y/max_y*h;
+
 	if( !fp ) fp = stdout;
 
 	fprintf( fp, "<?xml version='1.0' ?>\n" );
 	if( style_tag )
 		fprintf( fp, "<?xml-stylesheet type='text/css' href='%s' ?>\n", style_tag->val );
-	fprintf( fp, "<svg xmlns='http://www.w3.org/2000/svg' width='1000' height='1000'>\n" );
+	fprintf( fp, "<svg xmlns='http://www.w3.org/2000/svg' width='%i' height='%i'>\n", w+2*b, h+2*b );
 
-	arrow( 10, 510, 510, 510, fp );
-	arrow( 10, 510, 10, 10, fp );
+	float i;
+	arrow( b, h+b, w+b, h+b, fp );
+	if( reso_x > 0 ) for( i = b+reso_x; i < w+b; i += reso_x )
+		fprintf( fp, "<line x1='%f' y1='%i' x2='%f' y2='%i' />", i, h+b-5, i, h+b+5 );
+	arrow( b, h+b, b, b, fp );
+	if( reso_y > 0 ) for( i = h+b-reso_y; i > 0; i -= reso_y )
+		fprintf( fp, "<line x1='%i' y1='%f' x2='%i' y2='%f' />", b-5, i, b+5, i );
+
 	fprintf( fp, "<polyline class='curve' points='" );
 
 	container = _taglist;
@@ -133,9 +171,9 @@ int line_graph( struct tag *_taglist ) {
 	while( (container = container->next) ) {
 		if( container->parent && container->parent->id == parent_id && strcmp( container->class, "val" ) == 0 ) {
 			char *tmp;
-			float x = atof( container->val )/max_x*500+10;
+			float x = atof( container->val )/max_x*w+b;
 			if( !(tmp = strchr( container->val, delimiter )) ) return -1;
-			float y = 510-atof( tmp+1 )/max_y*500;
+			float y = h+b-atof( tmp+1 )/max_y*h;
 			fprintf( fp, "%f %f ", x, y );
 		}
 	}
@@ -148,11 +186,16 @@ int line_graph( struct tag *_taglist ) {
 }
 
 int bar_chart( struct tag *_taglist ) {
+	const char delimiter = ';';
+
 	FILE *fp = NULL;
 
 	struct tag *container = _taglist, *style_tag = 0;
 	int parent_id = container->id;
 
+	int number_of_values = 0;
+	int w = 0, h = 0, b = 0;
+	float reso_x = 0, reso_y = 0;
 	float min = 0, max = 0;
 	while( (container = container->next) ) {
 		if( container->parent && container->parent->id == parent_id ) {
@@ -160,23 +203,49 @@ int bar_chart( struct tag *_taglist ) {
 				fp = fopen( container->val, "w" );
 			if( strcmp( container->class, "style" ) == 0 )
 				style_tag = container;
+			if( strcmp( container->class, "size" ) == 0 ) {
+				w = atoi( container->val );
+				char *tmp; if( !(tmp = strchr( container->val, delimiter )) ) return -1;
+				h = atoi( tmp+1 );
+				if( container->attr ) b = atoi( container->attr );
+			}
+			if( strcmp( container->class, "reso" ) == 0 ) {
+				reso_x = atof( container->val );
+				char *tmp; if( !(tmp = strchr( container->val, delimiter )) ) return -1;
+				reso_y = atof( tmp+1 );
+			}
 			if( strcmp( container->class, "val" ) == 0 ) {
 				float val = atof( container->val );
 				if( ( min == 0 && val > 0 ) || val < min ) min = val;
 				if( val > max ) max = val;
+				number_of_values++;
 			}
 		}
 	}
 
+	if( w == 0 || h == 0 ) return -1;
+	max += reso_y/2;
+	reso_x = reso_x;
+	reso_y = reso_y/max*h;
+
 	if( !fp ) fp = stdout;
 
-	fprintf( fp, "<?xml version='1.0' ?>\n" );
-	if( style_tag )
-		fprintf( fp, "<?xml-stylesheet type='text/css' href='%s' ?>\n", style_tag->val );
-	fprintf( fp, "<svg xmlns='http://www.w3.org/2000/svg' width='1000' height='1000'>\n" );
+	fprintf( fp, "<?xml version='1.0' ?>\n<svg xmlns='http://www.w3.org/2000/svg' width='%i' height='%i'>\n", w+2*b, h+2*b );
+	// FIXME (and the other style thingies). Maybe have styles as tag attributes (both, in svg and in dataviz files); though that'd require
+	// that there were also tags (in dataviz files) for the coordinate axes.
+	if( style_tag ) {
+		FILE *stylefp = fopen( style_tag->val, "r" );
+		fprintf( fp, "<style>\n" );
+		int c; while( (c = fgetc( stylefp )) != EOF ) fputc( c, fp );
+		fprintf( fp, "</style>\n" );
+		fclose( stylefp );
+	}
 
-	arrow( 10, 510, 510, 510, fp );
-	arrow( 10, 510, 10, 10, fp );
+	float i;
+	arrow( b, h+b, w+b, h+b, fp );
+	arrow( b, h+b, b, b, fp );
+	if( reso_y > 0 ) for( i = h+b-reso_y; i > 0; i -= reso_y )
+		fprintf( fp, "<line x1='%i' y1='%f' x2='%i' y2='%f' />\n", b-5, i, b+5, i );
 
 	container = _taglist;
 
@@ -184,9 +253,9 @@ int bar_chart( struct tag *_taglist ) {
 	while( (container = container->next) ) {
 		if( container->parent && container->parent->id == parent_id && strcmp( container->class, "val" ) == 0 ) {
 			float x, y;
-			x = (id+1)*80;
-			y = 510-atof( container->val )/max*500;
-			fprintf( fp, "<line id='%i' class='bar bar-%i %s' x1='%f' y1='%f' x2='%f' y2='%f' />\n", id, id, (container->attr ? container->attr : ""), x, (float)510, x, y );
+			x = b + (id + (1-reso_x)*0.5)*w/number_of_values;
+			y = atof( container->val )/max*h;
+			fprintf( fp, "<rect id='%i' class='bar bar-%i %s' x='%f' y='%f' width='%f' height='%f' />\n", id, id, (container->attr ? container->attr : ""), x, h+b-y, reso_x*w/number_of_values, y );
 			id++;
 		}
 	}
